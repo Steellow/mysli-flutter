f7446e69dd8f860ac8507234d35b5874fcd8653b
Steellow <hankilapio@gmail.com>
2021-05-07 15:04:03 +0300

    Hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mysli',
       theme: ThemeData(
         primarySwatch: Colors.red,

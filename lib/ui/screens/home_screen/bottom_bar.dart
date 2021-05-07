4cc608368551382dcd3ebc6a053443a129a4c08d
Steellow <hankilapio@gmail.com>
2021-05-07 16:26:42 +0300

    Add feature to add items

--- lib/ui/screens/home_screen/bottom_bar.dart
+++ lib/ui/screens/home_screen/bottom_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mysli/logic/cubit/itemlist_cubit.dart';
+import 'package:mysli/model/item.dart';
 
 class BottomBar extends StatefulWidget {
   @override
@@ -6,15 +9,23 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
       child: TextField(
+        controller: _controller,
+        textInputAction: TextInputAction.go,
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           labelText: 'Add items',
         ),
+        onSubmitted: (value) {
+          BlocProvider.of<ItemlistCubit>(context).addItem(Item(value));
+          _controller.clear();
+        },
       ),
     );
   }

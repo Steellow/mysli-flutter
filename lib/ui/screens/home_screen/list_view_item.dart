65b14881661b912a6a0b102f2cc070fa2dd7aa6e
Steellow <hankilapio@gmail.com>
2021-05-08 18:00:10 +0300

    Convert to stateless widget

--- lib/ui/screens/home_screen/list_view_item.dart
+++ lib/ui/screens/home_screen/list_view_item.dart
@@ -3,24 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mysli/logic/cubit/itemlist_cubit.dart';
 import 'package:mysli/model/item.dart';
 
-class ListViewItem extends StatefulWidget {
+class ListViewItem extends StatelessWidget {
   final int index;
 
   const ListViewItem({Key key, @required this.index}) : super(key: key);
 
   @override
-  _ListViewItemState createState() => _ListViewItemState();
-}
-
-class _ListViewItemState extends State<ListViewItem> {
-  @override
   Widget build(BuildContext context) {
     return BlocBuilder<ItemlistCubit, ItemlistState>(
       buildWhen: (oldState, newState) {
-        if (widget.index >= newState.items.length) {
+        if (index >= newState.items.length) {
           // Comparing item at index which doesn't exist results in errors, so first we check if there is an item in that index
           return false;
-        } else if (oldState.items[widget.index] == newState.items[widget.index]) {
+        } else if (oldState.items[index] == newState.items[index]) {
           return false;
         } else {
           return true;
@@ -28,7 +23,7 @@ class _ListViewItemState extends State<ListViewItem> {
       },
       builder: (context, state) {
         print("ListViewItem: BlocBuilder builder function called");
-        Item item = state.items[widget.index];
+        Item item = state.items[index];
         return CheckboxListTile(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -50,9 +45,7 @@ class _ListViewItemState extends State<ListViewItem> {
           ),
           value: item.ticked,
           onChanged: (bool newValue) {
-            setState(() {
-              BlocProvider.of<ItemlistCubit>(context).tickItem(widget.index, newValue);
-            });
+            BlocProvider.of<ItemlistCubit>(context).tickItem(index, newValue);
           },
         );
       },
